2325e3c924c650e062d1cae51fb8033941878ec3
Israel Santos <santosjohnisrael@gmail.com>
2025-03-22 18:28:34 +0800

    upd9s

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,30 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'jids-portfolio';
   dpLink: string = 'assets/images/dp.jpg';
+
+  word1Value: string = "Stuff ";
+  animateClass: string = '';
+
+  word1Arr: string[] = ["Stuff ", "Web stuff", "Web Design ", "UI/UX", "Graphic Design ", "whatever."];
+
+  currentIndex: number = 0;
+
+  ngOnInit() {
+    this.startLoop();
+  }
+
+  startLoop() {
+      setTimeout(() => {
+      this.animateClass = '';
+      setTimeout(() => {
+        this.currentIndex = (this.currentIndex + 1) % this.word1Arr.length; 
+        this.word1Value = this.word1Arr[this.currentIndex];
+        this.animateClass = 'word';
+      }, 10);
+
+        this.startLoop();
+
+    }, 1600);
+  }
+
 }
